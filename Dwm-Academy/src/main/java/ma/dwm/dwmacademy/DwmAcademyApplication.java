0ee2aad2da9e3a92a29e23da314b64ea97f93926
amachlou <amachlou>
2021-06-08 00:39:46 +0100

    PFE-5 Define web project back-end structure

---
+++ Dwm-Academy/src/main/java/ma/dwm/dwmacademy/DwmAcademyApplication.java
@@ -0,0 +1,13 @@
+package ma.dwm.dwmacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwmAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwmAcademyApplication.class, args);
+	}
+
+}
---
+++ Dwm-Academy/src/test/java/ma/dwm/dwmacademy/DwmAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.dwm.dwmacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwmAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
