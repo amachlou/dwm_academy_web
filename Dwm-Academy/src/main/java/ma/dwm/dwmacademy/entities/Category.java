773e9a9480cbc1d88ed52dc7c781918fb2cbf8a3
amachlou <amachlou>
2021-06-11 18:10:19 +0100

    PFE-5 Define web project back-end structure.. Test the whole funnel

--- Dwm-Academy/src/main/java/ma/dwm/dwmacademy/entities/Category.java
+++ Dwm-Academy/src/main/java/ma/dwm/dwmacademy/entities/Category.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,7 @@ public class Category extends BaseEntity {
 	@Column(nullable = false)
 	private String name;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "category")
 	private List<Course> list_courses;
 	
--- Dwm-Academy/src/main/java/ma/dwm/dwmacademy/web/CategoryController.java
+++ Dwm-Academy/src/main/java/ma/dwm/dwmacademy/web/CategoryController.java
@@ -3,6 +3,8 @@ package ma.dwm.dwmacademy.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import ma.dwm.dwmacademy.entities.Category;
 import ma.dwm.dwmacademy.repositories.ICategoryRepository;
 
 @RestController
+//@Controller
 @RequestMapping(path = "/api2")
 public class CategoryController {
 	
@@ -18,7 +21,9 @@ public class CategoryController {
 	ICategoryRepository categoryRepository;
 	
 	@GetMapping(path = "/categories")
-	public List<Category> getCategories(){
+	public List<Category> getCategories(Model model){
+		model.addAttribute("categories", categoryRepository.findAll());
+//		return "view"; //categoryRepository.findAll();
 		return categoryRepository.findAll();
 	}
 
