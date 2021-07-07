dc4c0ea2557746b7b5a795ac88c7b1c1742bb3df
amachlou <amachlou>
2021-07-07 17:30:01 +0100

    Ignore validation

--- Dwm-Academy/src/main/java/ma/dwm/dwmacademy/entities/User.java
+++ Dwm-Academy/src/main/java/ma/dwm/dwmacademy/entities/User.java
@@ -21,60 +21,61 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ma.dwm.dwmacademy.utils.enum_gender;
-import ma.dwm.dwmacademy.utils.enum_userType;
+import ma.dwm.dwmacademy.utils.Enum_gender;
+import ma.dwm.dwmacademy.utils.Enum_userType;
 
 @Entity
 @Table(name = "USERS")
 @Data @NoArgsConstructor @AllArgsConstructor
 public class User extends BaseEntity {
 	
-	@NotBlank(message = "First name is mandatory")
+//	@NotBlank(message = "First name is mandatory")
 	@Size(min = 4, max = 50, message = "First name length should be betweeen 4 to 50 caracters")
 	@Column(nullable = false, length = 50)
 	private String first_name;
 	
-	@NotBlank(message = "Last name is mandatory")
-	@Size(min = 4, max = 50, message = "Last name length should be betweeen 4 to 50 caracters")
+//	@NotBlank(message = "Last name is mandatory")
+//	@Size(min = 4, max = 50, message = "Last name length should be betweeen 4 to 50 caracters")
 	@Column(nullable = false, length = 50)
 	private String last_name;
 	
-	@NotBlank(message = "Email is mandatory")
-	@Size(min = 4, max = 100, message = "First name length should be betweeen 4 to 100 caracters")
+//	@NotBlank(message = "Email is mandatory")
+//	@Size(min = 4, max = 100, message = "First name length should be betweeen 4 to 100 caracters")
 	@Email(message = "Email should be valid")
 	@Column(nullable = false, length = 100)
 	private String email;
 	
 	private String title;
 	
-	@NotBlank(message = "Date of birth is mandatory")
+//	@NotBlank(message = "Date of birth is mandatory")
 	@Temporal(TemporalType.DATE)
 	private Date date_birth;
 	
-	@NotBlank(message = "Password is mandatory")
-	@Size(min = 4, max = 50, message = "Password lenth should be betweeen 4 to 50 caracters")
+//	@NotBlank(message = "Password is mandatory")
+//	@Size(min = 4, max = 50, message = "Password lenth should be betweeen 4 to 50 caracters")
 	@Column(nullable = false)
 	private String password;
 	
-	@NotBlank(message = "Password confirmation is mandatory")
-	@Size(min = 4, max = 50, message = "Password confirmation length should be betweeen 4 to 50 caracters")
+	@Transient
+	private String current_password;
+	
+//	@NotBlank(message = "Password confirmation is mandatory")
+//	@Size(min = 4, max = 50, message = "Password confirmation length should be betweeen 4 to 50 caracters")
 	@Transient
 	private String confirm_password;
 	
-	@NotEmpty(message = "Type is mandatory")
+//	@NotEmpty(message = "Type is mandatory")
 	@Enumerated(EnumType.STRING)
-	private enum_userType type;
+	private Enum_userType type;
 	
-	@NotEmpty(message = "Type is mandatory")
+//	@NotEmpty(message = "Type is mandatory")
 	@Enumerated(EnumType.STRING)
-	private enum_gender gender;
+	private Enum_gender gender;
 	
 	private String photo;
 	private boolean active = true;
